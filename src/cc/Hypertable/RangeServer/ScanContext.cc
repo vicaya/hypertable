0d2e6ba407ac8780c4e6030394cf96379c328e62
nuggetwheat <nuggetwheat@01261837-d133-0410-8dcb-6792eb736156>
2007-09-11 19:47:26 +0000

    Got ScanContext.familyInfo setup properly
    git-svn-id: https://hypertable.googlecode.com/svn/trunk@183 01261837-d133-0410-8dcb-6792eb736156

--- src/cc/Hypertable/RangeServer/ScanContext.cc
+++ src/cc/Hypertable/RangeServer/ScanContext.cc
@@ -32,62 +32,79 @@ const uint64_t ScanContext::END_OF_TIME = (uint64_t)-1;
  */
 void ScanContext::initialize(uint64_t ts, ScanSpecificationT *ss, SchemaPtr &sp) {
   Schema::ColumnFamily *columnFamily;
+  uint32_t cellLimit = 0;
 
   timestamp = ts;
   spec = ss;
 
-  if (spec == 0) {
+  assert(sp);
+
+  if (spec == 0)
     memset(familyMask, true, 256*sizeof(bool));
-    memset(familyInfo, 0, 256*sizeof(CellFilterInfoT));
-    return;
-  }
-  if (sp) {
-    schemaPtr = sp;
+  else {
     memset(familyMask, false, 256*sizeof(bool));
-    if (spec->columns.size() > 0) {
-      for (std::vector<const char *>::const_iterator iter = spec->columns.begin(); iter != spec->columns.end(); iter++) {
-	columnFamily = schemaPtr->GetColumnFamily(*iter);
-	assert(columnFamily);
+    cellLimit = spec->cellLimit;
+  }
 
-	familyMask[columnFamily->id] = true;
-	if (columnFamily->expireTime == 0)
-	  familyInfo[columnFamily->id].cutoffTime == 0;
-	else
-	  familyInfo[columnFamily->id].cutoffTime == timestamp - (columnFamily->expireTime * 1000000);
+  memset(familyInfo, 0, 256*sizeof(CellFilterInfoT));
+
+  schemaPtr = sp;
+  if (spec && spec->columns.size() > 0) {
+    for (std::vector<const char *>::const_iterator iter = spec->columns.begin(); iter != spec->columns.end(); iter++) {
+      columnFamily = schemaPtr->GetColumnFamily(*iter);
+      assert(columnFamily);
+
+      familyMask[columnFamily->id] = true;
+      if (columnFamily->expireTime == 0)
+	familyInfo[columnFamily->id].cutoffTime == 0;
+      else
+	familyInfo[columnFamily->id].cutoffTime == timestamp - (columnFamily->expireTime * 1000000);
+      if (cellLimit == 0)
 	familyInfo[columnFamily->id].cellLimit = columnFamily->cellLimit;
-      }
-    }
-    else {
-      list<Schema::AccessGroup *> *agList = schemaPtr->GetAccessGroupList();
-
-      familyMask[0] = true;  // ROW_DELETE records have 0 column family, so this allows them to pass through
-      for (list<Schema::AccessGroup *>::iterator agIter = agList->begin(); agIter != agList->end(); agIter++) {
-	for (list<Schema::ColumnFamily *>::iterator cfIter = (*agIter)->columns.begin(); cfIter != (*agIter)->columns.end(); cfIter++) {
-	  if ((*cfIter)->id == 0) {
-	    error = Error::RANGESERVER_SCHEMA_INVALID_CFID;
-	    return;
-	  }
-	  familyMask[(*cfIter)->id] = true;
-	  if ((*cfIter)->expireTime == 0)
-	    familyInfo[(*cfIter)->id].cutoffTime == 0;
-	  else
-	    familyInfo[(*cfIter)->id].cutoffTime == timestamp - ((*cfIter)->expireTime * 1000000);
-	  familyInfo[(*cfIter)->id].cellLimit = (*cfIter)->cellLimit;
-	}
+      else {
+	if (columnFamily->cellLimit == 0)
+	  familyInfo[columnFamily->id].cellLimit = cellLimit;
+	else
+	  familyInfo[columnFamily->id].cellLimit = (cellLimit < columnFamily->cellLimit) ? cellLimit : columnFamily->cellLimit;
       }
     }
   }
   else {
-    memset(familyMask, true, 256*sizeof(bool));
-    memset(familyInfo, 0, 256*sizeof(CellFilterInfoT));
+    list<Schema::AccessGroup *> *agList = schemaPtr->GetAccessGroupList();
+
+    familyMask[0] = true;  // ROW_DELETE records have 0 column family, so this allows them to pass through
+    for (list<Schema::AccessGroup *>::iterator agIter = agList->begin(); agIter != agList->end(); agIter++) {
+      for (list<Schema::ColumnFamily *>::iterator cfIter = (*agIter)->columns.begin(); cfIter != (*agIter)->columns.end(); cfIter++) {
+	if ((*cfIter)->id == 0) {
+	  error = Error::RANGESERVER_SCHEMA_INVALID_CFID;
+	  return;
+	}
+	familyMask[(*cfIter)->id] = true;
+	if ((*cfIter)->expireTime == 0)
+	  familyInfo[(*cfIter)->id].cutoffTime == 0;
+	else
+	  familyInfo[(*cfIter)->id].cutoffTime == timestamp - ((*cfIter)->expireTime * 1000000);
+
+	if (cellLimit == 0)
+	  familyInfo[(*cfIter)->id].cellLimit = (*cfIter)->cellLimit;
+	else {
+	  if ((*cfIter)->cellLimit == 0)
+	    familyInfo[(*cfIter)->id].cellLimit = cellLimit;
+	  else
+	    familyInfo[(*cfIter)->id].cellLimit = (cellLimit < (*cfIter)->cellLimit) ? cellLimit : (*cfIter)->cellLimit;
+	}
+      }
+    }
   }
 
   /**
    * Create Start Key and End Key
    */
-  if (*spec->startRow != 0)
-    startKeyPtr = CreateKey(FLAG_INSERT, spec->startRow, 0, 0, 0LL);
-  if (*spec->endRow != 0)
-    endKeyPtr = CreateKey(FLAG_INSERT, spec->endRow, 0xFF, 0, 0LL);
+  if (spec) {
+    if (*spec->startRow != 0)
+      startKeyPtr = CreateKey(FLAG_INSERT, spec->startRow, 0, 0, 0LL);
+    if (*spec->endRow != 0)
+      endKeyPtr = CreateKey(FLAG_INSERT, spec->endRow, 0xFF, 0, 0LL);
+  }
 
 }
