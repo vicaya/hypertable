1fd0a77355c9ddc8c0f562287012baceea13364f
nuggetwheat <nuggetwheat@01261837-d133-0410-8dcb-6792eb736156>
2007-12-19 17:20:47 +0000

    forgot to add rsdump tool in previous checkin
    git-svn-id: https://hypertable.googlecode.com/svn/trunk@480 01261837-d133-0410-8dcb-6792eb736156

---
+++ src/cc/Tools/rsdump/rsdump.cc
@@ -0,0 +1,150 @@
+/**
+ * Copyright (C) 2007 Doug Judd (Zvents, Inc.)
+ * 
+ * This file is part of Hypertable.
+ * 
+ * Hypertable is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * 
+ * Hypertable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+extern "C" {
+#include <netdb.h>
+}
+
+#include "Common/Error.h"
+#include "Common/InetAddr.h"
+#include "Common/Logger.h"
+#include "Common/Properties.h"
+#include "Common/System.h"
+#include "Common/Usage.h"
+
+#include "AsyncComm/ApplicationQueue.h"
+#include "AsyncComm/Comm.h"
+#include "AsyncComm/ConnectionManager.h"
+#include "AsyncComm/ReactorFactory.h"
+
+#include "DfsBroker/Lib/Client.h"
+
+#include "Hyperspace/Session.h"
+#include "Hypertable/Lib/MasterClient.h"
+#include "Hypertable/Lib/RangeServerClient.h"
+
+using namespace Hypertable;
+using namespace std;
+
+namespace {
+
+  const char *usage[] = {
+    "usage: rsdump [options]",
+    "",
+    "  options:",
+    "    --config=<file>  Read configuration from <file>.  The default config file",
+    "                     is \"conf/hypertable.cfg\" relative to the toplevel",
+    "                     install directory",
+    "    --host=<name>    Connect to the server running on host <name>",
+    "    --port=<n>       Connect to the server running on port <n>",
+    "    --help           Display this help text and exit",
+    "    --verbose,-v     Display 'true' if up, 'false' otherwise",
+    "",
+    "  This program instructs the range server to dump out status to it's log.",
+    "",
+    (const char *)0
+  };
+  
+}
+
+
+/**
+ *
+ */
+int main(int argc, char **argv) {
+  string configFile = "";
+  string hostName = "";
+  int port = 0;
+  PropertiesPtr propsPtr;
+  struct sockaddr_in addr;
+  const char *hostProperty = 0;
+  const char *portProperty = 0;
+  const char *portStr = 0;
+  Comm *comm = 0;
+  ConnectionManagerPtr connManagerPtr;
+  Hyperspace::SessionPtr hyperspacePtr;
+  RangeServerClient *rangeServer;
+  ApplicationQueuePtr appQueuePtr;
+  int error;
+  bool verbose = false;
+
+  System::initialize(argv[0]);
+  ReactorFactory::initialize((uint16_t)System::get_processor_count());
+
+  for (int i=1; i<argc; i++) {
+    if (!strncmp(argv[i], "--config=", 9))
+      configFile = &argv[i][9];
+    else if (!strncmp(argv[i], "--host=", 7))
+      hostName = &argv[i][7];
+    else if (!strncmp(argv[i], "--port=", 7))
+      portStr = &argv[i][7];
+    else if (!strcmp(argv[i], "--verbose") || !strcmp(argv[i], "-v"))
+      verbose = true;
+    else
+      Usage::dump_and_exit(usage);
+  }
+
+  if (configFile == "")
+    configFile = System::installDir + "/conf/hypertable.cfg";
+
+  propsPtr.reset( new Properties(configFile) );
+
+  if (hostName == "")
+    hostName = "localhost";
+  portProperty = "Hypertable.RangeServer.port";
+
+  {
+    if (hostName == "")
+      hostName = propsPtr->getProperty(hostProperty, "localhost");
+    
+    if (portStr != 0)
+      propsPtr->setProperty(portProperty, portStr);
+
+    port = propsPtr->getPropertyInt(portProperty, 0);
+    if (port == 0 || port < 1024 || port >= 65536) {
+      LOG_VA_ERROR("%s not specified or out of range : %d", portProperty, port);
+      return 1;
+    }
+
+    if (!InetAddr::initialize(&addr, hostName.c_str(), (uint16_t)port))
+      goto abort;
+  }
+
+  propsPtr->setProperty("silent", "true");
+
+  comm = new Comm();
+  connManagerPtr = new ConnectionManager(comm);
+
+  //connManagerPtr->set_quiet_mode(true);
+
+  connManagerPtr->add(addr, 30, "Range Server");
+  if (!connManagerPtr->wait_for_connection(addr, 2))
+    goto abort;
+  rangeServer = new RangeServerClient(comm, 30);
+  if ((error = rangeServer->dump_stats(addr)) != Error::OK)
+    goto abort;
+  return 0;
+
+ abort:
+  return 1;
+}
