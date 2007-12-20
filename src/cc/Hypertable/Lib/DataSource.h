ce02299d33dd1562fff581a41f87f7ef0b5e9775
nuggetwheat <nuggetwheat@01261837-d133-0410-8dcb-6792eb736156>
2007-12-20 01:23:30 +0000

    New file forgotten in last checkin
    git-svn-id: https://hypertable.googlecode.com/svn/trunk@483 01261837-d133-0410-8dcb-6792eb736156

---
+++ src/cc/Hypertable/Lib/DataSource.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2007 Doug Judd (Zvents, Inc.)
+ * 
+ * This file is part of Hypertable.
+ * 
+ * Hypertable is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * 
+ * Hypertable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef HYPERTABLE_DATASOURCE_H
+#define HYPERTABLE_DATASOURCE_H
+
+extern "C" {
+#include <stdint.h>
+}
+
+#include "KeySpec.h"
+
+using namespace Hypertable;
+
+namespace Hypertable {
+
+  class DataSource {
+  public:
+    virtual ~DataSource() { return; }
+    virtual bool next(uint32_t *type_flagp, uint64_t *timestampp, KeySpec *keyp, uint8_t **valuep, uint32_t *value_lenp, uint32_t *consumedp);
+  };
+
+}
+
+#endif // HYPERTABLE_DATASOURCE_H
