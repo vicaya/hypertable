dfd14bde5d53b6f90bc533a5b867b59338904e9d
Luke Lu <hypertable@vicaya.com>
2008-03-28 22:28:18 -0700

    First cut of meta log API

---
+++ src/cc/Common/String.cc
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2008 Luke Lu (Zvents, Inc.)
+ *
+ * This file is part of Hypertable.
+ *
+ * Hypertable is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * Hypertable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hypertable. If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "String.h"
+
+namespace Hypertable {
+
+String format(const char *fmt, ...) {
+  char buf[512];
+  int n, size = sizeof(buf);
+  char *p = buf;
+  va_list ap;
+
+  do {
+    va_start(ap, fmt);
+    n = vsnprintf(p, size, fmt, ap);
+    va_end(ap);
+    if (n > -1 && n < size) return p; // worked!
+    if (n > -1)         // glibc 2.1
+      size = n + 1;     //   exactly what's needed
+    else                // glibc 2.0
+      size *= 2;        //   double the size and try again
+    if ((p = (char *) malloc(size)) == NULL) return "Out of memory";
+  } while (true);
+}
+
+} // namespace Hypertable
---
+++ src/cc/Hypertable/Lib/tests/MetaLogApiTest.cc
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2008 Luke Lu (Zvents, Inc.)
+ *
+ * This file is part of Hypertable.
+ *
+ * Hypertable is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * Hypertable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hypertable. If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <stdlib.h>
+#include "Hypertable/Lib/MetaLogReader.h"
+#include "Hypertable/Lib/MetaLogEntryFactory.h"
+#include <boost/scoped_ptr.hpp>
+
+using namespace Hypertable;
+using namespace MetaLogEntryFactory;
+using boost::scoped_ptr;
+
+// just get api to compile first
+
+MetaLogEntry *
+MetaLogEntryFactory::new_from_payload(int type, const void *buf, size_t len) {
+  return NULL;
+}
+
+namespace {
+
+struct NullEntry : public MetaLogEntry {
+  virtual void write(DynamicBuffer &)  {}
+  virtual void read(const void *buf, size_t len) {}
+  virtual bool is_valid() { return true; }
+  virtual int get_type() { return NULL_TEST; }
+};
+
+struct NullLog : public MetaLog {
+  virtual void write(MetaLogEntry *) {}
+  virtual void close() {}
+  virtual void compact() {}
+};
+
+struct NullReader : public MetaLogReader {
+  virtual ScanEntry *next(ScanEntry *) { return NULL; }
+  virtual ScanEntry *last(ScanEntry *) { return NULL; }
+};
+
+void
+null_test() {
+  scoped_ptr<MetaLog> metalog(new NullLog);
+  scoped_ptr<MetaLogReader> reader(new NullReader);
+  scoped_ptr<MetaLogEntry> log_entry(new NullEntry);
+
+  metalog->write(log_entry.get());
+  metalog->close();
+  metalog->compact();
+
+  reader->read();
+  reader->read_last();
+}
+
+} // local namespace
+
+int
+main() {
+  null_test();
+  return 0;
+}
