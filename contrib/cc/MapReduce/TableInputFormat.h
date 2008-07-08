004dc8ab64fb1d1d7ee07f7bc8d3af2c2965b5a5
Mateusz Berezecki <mateuszb@gmail.com>
2008-07-08 16:25:38 +0200

    Revert "fixed jni header files so that signatures match now packaged version of the java part" because of accidental file removal. This reverts commit 7fba3015ab3050e5740e9801e3c1709de6bb6ef3.

---
+++ contrib/cc/MapReduce/TableInputFormat.h
@@ -0,0 +1,23 @@
+#include <jni.h>
+/* Header for class TableInputFormat */
+
+namespace Mapreduce {
+#ifndef _Included_TableInputFormat
+#define _Included_TableInputFormat
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     TableInputFormat
+ * Method:    getRangeVector
+ * Signature: (Ljava/lang/String;)[Ljava/lang/String;
+ */
+JNIEXPORT jobjectArray JNICALL Java_TableInputFormat_getRangeVector
+  (JNIEnv *, jobject, jstring);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+}
+
