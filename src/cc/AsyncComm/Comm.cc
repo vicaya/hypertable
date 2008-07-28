7a6faf4ff3d1411143948d0a6a92eb2d878889af
Doug Judd <doug@zvents.com>
2008-07-28 01:14:42 -0700

    Fixed TOS setting on the mac

--- src/cc/AsyncComm/Comm.cc
+++ src/cc/AsyncComm/Comm.cc
@@ -30,6 +30,7 @@ using namespace std;
 extern "C" {
 #if defined(__APPLE__)
 #include <arpa/inet.h>
+#include <netinet/ip.h>
 #endif
 #include <errno.h>
 #include <fcntl.h>
@@ -272,10 +273,16 @@ Comm::create_datagram_receive_socket(struct sockaddr_in *addr, int tos,
   }
 
   if (tos) {
-    int opt = tos;
+    int opt;
+#if defined(__linux__)
+    opt = tos;
     setsockopt(sd, SOL_IP, IP_TOS, &opt, sizeof(opt));
     opt = tos;
     setsockopt(sd, SOL_SOCKET, SO_PRIORITY, &opt, sizeof(opt));    
+#elif defined(__APPLE__)
+    opt = IPTOS_LOWDELAY;       /* see <netinet/in.h> */
+    setsockopt(sd, IPPROTO_IP, IP_TOS, &opt, sizeof(opt));
+#endif
   }
 
   // bind socket
