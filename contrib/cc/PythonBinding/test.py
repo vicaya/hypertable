5afddc27839558f16830283e20874bf327c6f962
Mateusz Berezecki <mateuszb@gmail.com>
2008-08-01 00:13:00 +0200

    added write support for the python bindings

--- contrib/cc/PythonBinding/test.py
+++ contrib/cc/PythonBinding/test.py
@@ -1,28 +1,41 @@
 import ht
 
 # create client
-client = ht.Client("ht", "/opt/hypertable/0.9.0.7/conf/hypertable.cfg")
+client = ht.Client("/opt/hypertable/0.9.0.7/")
 
 # open the table
-table = client.open_table("METADATA")
+table = client.open_table("my_table")
 
 # create scan specification
-scan_spec = ht.ScanSpec()
-scan_spec.start_row = "\x00"
-scan_spec.end_row = "\xff\xff"
-scan_spec.start_row_inclusive = True
-scan_spec.end_row_inclusive = True
-scan_spec.max_versions = 1
+scan_spec_builder = ht.ScanSpecBuilder()
+scan_spec_builder.add_row_interval("\x00", True, "\xff\xff", True)
+scan_spec_builder.add_column("a")
 
 # initialize the scanner with it
-scanner = table.create_scanner(scan_spec)
+scanner = table.create_scanner(scan_spec_builder)
 
 # create cell holding consecutive values from the scanner
 cell = ht.Cell()
 
 # scan it!
-while scanner.next(cell):
+#while scanner.next(cell):
   # note that instead of cell.value and cell.value_len fields
   # you get single cell.value() function returning the value from the cell
-  print "%s:%s %s" % (cell.row_key, cell.column_family, cell.value())
+  #print "%s:%s %s" % (cell.row_key, cell.column_family, cell.value())
 
+
+# create table mutator
+
+mutator = table.create_mutator(10)
+
+families = ['a','b','c']
+
+for i in range(1,100000):
+  row = "row-%s" % (i)
+  family = families[i % 3]
+  qualifier = ""
+  value = "value:%s" % i
+
+  mutator.set(row, family, qualifier, value, len(value)) 
+
+mutator.flush()
