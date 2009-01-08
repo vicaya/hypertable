86395aff7a1dbe5ed47868072a0eda55bb7fc738
Luke Lu <hypertable@vicaya.com>
2009-01-08 00:38:41 -0800

    Fixed a few issues with FindBoost.cmake - now uses the right way to check boost version for system library - now dies when any of the required libraries was not found

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -30,14 +30,8 @@ if (WIN32)
   set(Boost_LIB_DIAGNOSTIC_DEFINITIONS "-DBOOST_LIB_DIAGNOSTIC")
 endif ()
 
-set(BOOST_INCLUDE_PATH_DESCRIPTION "directory containing the boost include "
-    "files. E.g /usr/local/include/boost-1_34_1 or "
-    "c:\\boost\\include\\boost-1_34_1")
-
-set(BOOST_DIR_MESSAGE "Set the Boost_INCLUDE_DIR cmake cache entry to the "
-    "${BOOST_INCLUDE_PATH_DESCRIPTION}")
-
 set(BOOST_DIR_SEARCH $ENV{BOOST_ROOT})
+
 if (BOOST_DIR_SEARCH)
   file(TO_CMAKE_PATH ${BOOST_DIR_SEARCH} BOOST_DIR_SEARCH)
   set(BOOST_DIR_SEARCH ${BOOST_DIR_SEARCH}/include)
@@ -81,24 +75,18 @@ macro(FIND_BOOST_PARENT root includedir)
   # Note that the user may not have installed any libraries
   # so it is quite possible the library path may not exist.
   set(${root} ${includedir})
-  if (${${root}} MATCHES "boost-[0-9]+")
-    # Check for Boost 1.34
-    # which doesn't have a separate System Lib
-    set (Boost_HAS_SYSTEM_LIB true)
-    if (${${root}} MATCHES "boost-1_34")
-      message(STATUS "Boost 1_34 found. Disable Boost system library.")
-      set(Boost_HAS_SYSTEM_LIB  false)
-    endif()
+
+  if (${root} MATCHES "boost-[0-9]+")
     get_filename_component(${root} ${${root}} PATH)
   endif ()
 
-  if (${${root}} MATCHES "/include$")
+  if (${root} MATCHES "/include$")
     # Strip off the trailing "/include" in the path.
     get_filename_component(${root} ${${root}} PATH)
   endif ()
 endmacro(FIND_BOOST_PARENT root includedir)
 
-macro(FIND_BOOST_LIBRARY lib libname libroot)
+macro(FIND_BOOST_LIBRARY lib libname libroot required)
   set(${lib}_NAMES
     "boost_${libname}-mt"
     "boost_${libname}-gcc42-mt"
@@ -114,6 +102,10 @@ macro(FIND_BOOST_LIBRARY lib libname libroot)
     NAMES ${${lib}_NAMES}
     PATHS "${libroot}/lib" "${libroot}/lib64"
   )
+  if (required AND ${lib} MATCHES "NOTFOUND$")
+    message(FATAL_ERROR "required boost library: ${lib} not found")
+  endif ()
+
   mark_as_advanced(${lib})
 endmacro()
 
@@ -122,41 +114,71 @@ set(Boost_FOUND 0)
 
 # Now try to get the include and library path.
 if (Boost_INCLUDE_DIR)
+  try_run(BOOST_CHECK SHOULD_COMPILE
+          ${HYPERTABLE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
+          ${HYPERTABLE_SOURCE_DIR}/cmake/CheckBoost.cc
+          CMAKE_FLAGS -DINCLUDE_DIRECTORIES:STRING=${Boost_INCLUDE_DIR}
+          COMPILE_DEFINITIONS -v
+          OUTPUT_VARIABLE BOOST_TRY_OUT)
+  string(REGEX REPLACE ".*\n([0-9_]+).*" "\\1" BOOST_VERSION ${BOOST_TRY_OUT})
+  string(REGEX REPLACE ".*\ngcc version ([0-9.]+).*" "\\1" GCC_VERSION
+         ${BOOST_TRY_OUT})
+
+  if (GCC_VERSION)
+    message(STATUS "GCC version: ${GCC_VERSION}")
+  endif ()
+
+  message(STATUS "Boost version: ${BOOST_VERSION}")
+
+  if (NOT BOOST_CHECK STREQUAL "0")
+    message(FATAL_ERROR "Boost version not compatible")
+  endif ()
+
+  if (BOOST_VERSION MATCHES "1_34")
+    set(Boost_HAS_SYSTEM_LIB false)
+  else ()
+    set(Boost_HAS_SYSTEM_LIB true)
+  endif()
 
   message(STATUS "Boost include dir: ${Boost_INCLUDE_DIR}")
   FIND_BOOST_PARENT(Boost_PARENT ${Boost_INCLUDE_DIR})
 
   # Add boost libraries here
-  FIND_BOOST_LIBRARY(BOOST_THREAD_LIB thread ${Boost_PARENT})
-  FIND_BOOST_LIBRARY(BOOST_PROGRAM_OPTIONS_LIB program_options ${Boost_PARENT})
-  FIND_BOOST_LIBRARY(BOOST_IOSTREAMS_LIB iostreams ${Boost_PARENT})
-  FIND_BOOST_LIBRARY(BOOST_FILESYSTEM_LIB filesystem ${Boost_PARENT})
-  FIND_BOOST_LIBRARY(BOOST_PYTHON_LIB python ${Boost_PARENT})
+  FIND_BOOST_LIBRARY(BOOST_THREAD_LIB thread ${Boost_PARENT} true)
+  get_filename_component(Boost_LIBRARY_DIR ${BOOST_THREAD_LIB} PATH)
+  FIND_BOOST_LIBRARY(BOOST_PROGRAM_OPTIONS_LIB program_options
+                     ${Boost_PARENT} true)
+  FIND_BOOST_LIBRARY(BOOST_IOSTREAMS_LIB iostreams ${Boost_PARENT} true)
+  FIND_BOOST_LIBRARY(BOOST_FILESYSTEM_LIB filesystem ${Boost_PARENT} true)
+  FIND_BOOST_LIBRARY(BOOST_PYTHON_LIB python ${Boost_PARENT} false)
+
   if(Boost_HAS_SYSTEM_LIB)
-    FIND_BOOST_LIBRARY(BOOST_SYSTEM_LIB system ${Boost_PARENT})
+    FIND_BOOST_LIBRARY(BOOST_SYSTEM_LIB system ${Boost_PARENT} true)
   endif()
+
   if (NOT Boost_FIND_QUIETLY)
     message(STATUS "Boost thread lib: ${BOOST_THREAD_LIB}")
     message(STATUS "Boost program options lib: ${BOOST_PROGRAM_OPTIONS_LIB}")
     message(STATUS "Boost filesystem lib: ${BOOST_FILESYSTEM_LIB}")
     message(STATUS "Boost iostreams lib: ${BOOST_IOSTREAMS_LIB}")
     message(STATUS "Boost python lib: ${BOOST_PYTHON_LIB}")
+
     if(Boost_HAS_SYSTEM_LIB)
       message(STATUS "Boost system lib: ${BOOST_SYSTEM_LIB}")
     endif()
-    get_filename_component(Boost_LIBRARY_DIR ${BOOST_THREAD_LIB} PATH)
+
     message(STATUS "Boost lib dir: ${Boost_LIBRARY_DIR}")
   endif ()
 
   # BOOST_LIBS is our default boost libs.
-  if(Boost_HAS_SYSTEM_LIB)
-    set(BOOST_LIBS ${BOOST_IOSTREAMS_LIB} ${BOOST_PROGRAM_OPTIONS_LIB}
-      ${BOOST_FILESYSTEM_LIB} ${BOOST_THREAD_LIB} ${BOOST_SYSTEM_LIB})
-  else()
-    set(BOOST_LIBS ${BOOST_IOSTREAMS_LIB} ${BOOST_PROGRAM_OPTIONS_LIB}
+  set(BOOST_LIBS ${BOOST_IOSTREAMS_LIB} ${BOOST_PROGRAM_OPTIONS_LIB}
       ${BOOST_FILESYSTEM_LIB} ${BOOST_THREAD_LIB})
+
+  if(Boost_HAS_SYSTEM_LIB)
+    set(BOOST_LIBS ${BOOST_LIBS} ${BOOST_SYSTEM_LIB})
   endif()
-  if (EXISTS "${Boost_INCLUDE_DIR}")
+
+  if (EXISTS ${Boost_INCLUDE_DIR})
     set(Boost_INCLUDE_DIRS ${Boost_INCLUDE_DIR})
     # We have found boost. It is possible that the user has not
     # compiled any libraries so we set Boost_FOUND to be true here.
@@ -166,35 +188,13 @@ if (Boost_INCLUDE_DIR)
   if (Boost_LIBRARY_DIR AND EXISTS "${Boost_LIBRARY_DIR}")
     set(Boost_LIBRARY_DIRS ${Boost_LIBRARY_DIR})
   endif ()
-
-  try_run(BOOST_CHECK SHOULD_COMPILE
-          ${HYPERTABLE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
-          ${HYPERTABLE_SOURCE_DIR}/cmake/CheckBoost.cc
-          CMAKE_FLAGS -DINCLUDE_DIRECTORIES:STRING=${Boost_INCLUDE_DIRS}
-          COMPILE_DEFINITIONS -v
-          OUTPUT_VARIABLE BOOST_TRY_OUT)
-  string(REGEX REPLACE ".*\n([0-9_]+).*" "\\1" BOOST_VERSION ${BOOST_TRY_OUT})
-  string(REGEX REPLACE ".*\ngcc version ([0-9.]+).*" "\\1" GCC_VERSION
-         ${BOOST_TRY_OUT})
-  message(STATUS "Boost version: ${BOOST_VERSION}")
-
-  if (NOT BOOST_CHECK STREQUAL "0")
-    message(FATAL_ERROR "Boost version not compatible")
-  endif ()
-
-  if (GCC_VERSION)
-    message(STATUS "gcc version: ${GCC_VERSION}")
-  endif ()
-
 endif ()
 
 if (NOT Boost_FOUND)
-  if (NOT Boost_FIND_QUIETLY)
-    message(STATUS "Boost was not found. ${BOOST_DIR_MESSAGE}")
-  else ()
-    if (Boost_FIND_REQUIRED)
-      message(FATAL_ERROR "Boost was not found. ${BOOST_DIR_MESSAGE}")
-    endif ()
+  if (Boost_FIND_REQUIRED)
+    message(FATAL_ERROR "Required boost libraries not found")
+  elseif (NOT Boost_FIND_QUIETLY)
+    message(STATUS "Required boost libraries not found")
   endif ()
 endif ()
 
