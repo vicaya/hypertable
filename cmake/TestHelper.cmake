bff9f15006c5a78ba6dbaf6969b07f1586034d7d
Luke Lu <hypertable@vicaya.com>
2009-01-09 14:39:14 -0800

    Renamed top-level 'test' directory to 'tests' for some platforms - Linux seems to propagate mtime up the directory hierarchy making   make test thinking the target 'test' is up to date and do nothing

--- cmake/TestHelper.cmake
+++ cmake/TestHelper.cmake
@@ -1,11 +1,17 @@
-set(HT_INSTALLED_SERVERS
+set(INSTALLED_SERVERS
   ${INSTALL_DIR}/bin/Hyperspace.Master
   ${INSTALL_DIR}/bin/Hypertable.Master
   ${INSTALL_DIR}/bin/Hypertable.RangeServer
-  ${INSTALL_DIR}/bin/ThriftBroker
   ${INSTALL_DIR}/bin/localBroker
-  ${INSTALL_DIR}/bin/kosmosBroker
 )
+
+if (Thrift_FOUND)
+  set(INSTALLED_SERVERS ${INSTALLED_SERVERS} ${INSTALL_DIR}/bin/ThriftBroker)
+endif()
+
+if (Kfs_FOUND)
+  set(INSTALLED_SERVERS ${INSTALLED_SERVERS} ${INSTALL_DIR}/bin/kosmosBroker)
+endif()
 
 set(TEST_SERVERS_STARTED ${HYPERTABLE_BINARY_DIR}/test-servers-started)
 
@@ -13,7 +19,7 @@ add_custom_command(
   OUTPUT    ${TEST_SERVERS_STARTED}
   COMMAND   ${HYPERTABLE_SOURCE_DIR}/bin/start-test-servers.sh
   ARGS      ${INSTALL_DIR} ${TEST_SERVERS_STARTED}
-  DEPENDS   ${HT_INSTALLED_SERVERS}
+  DEPENDS   ${INSTALLED_SERVERS}
   COMMENT   "Starting test servers"
 )
 
@@ -29,4 +35,4 @@ endmacro()
 # custom target must be globally unique to support IDEs like Xcode, VS etc.
 add_test_target(alltests ${HYPERTABLE_BINARY_DIR})
 add_test_target(coretests ${HYPERTABLE_BINARY_DIR}/src)
-add_test_target(moretests ${HYPERTABLE_BINARY_DIR}/test/integration)
+add_test_target(moretests ${HYPERTABLE_BINARY_DIR}/tests/integration)
