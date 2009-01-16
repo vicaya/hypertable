vicaya/hypertable

commit a58dce91b4059b022921287a43d7afb669380d0b

[issue 206] Fixed problem where CommitLog fd gets into bad state
- Fixed race condition in RangeServer::load_range (reported by Phoenix)
- Wrapped compaction logic in try/catch block to ensure maintenance bit gets cleared on exception

src/cc/Hypertable/Lib/CommitLog.cc
@@ -112,6 +112,7 @@ CommitLog::initialize(Filesystem *fs, const String &log_dir,
   catch (Hypertable::Exception &e) {
     HT_ERRORF("Problem initializing commit log '%s' - %s (%s)",
               m_log_dir.c_str(), e.what(), Error::get_text(e.code()));
+    m_fd = -1;
     throw;
   }
 }
@@ -128,6 +129,17 @@ int64_t CommitLog::get_timestamp() {
 int CommitLog::write(DynamicBuffer &buffer, int64_t revision) {
   int error;
   BlockCompressionHeaderCommitLog header(MAGIC_DATA, revision);
+
+  if (m_fd == -1) {
+    try {
+      m_fd = m_fs->create(m_cur_fragment_fname, true, 8192, 3, 67108864);
+    }
+    catch (Hypertable::Exception &e) {
+      HT_ERRORF("Problem creating commit log fragment file '%s' - %s (%s)",
+                m_log_dir.c_str(), e.what(), Error::get_text(e.code()));
+      return e.code();
+    }
+  }
 
   /**
    * Compress and write the commit block
@@ -155,6 +167,17 @@ int CommitLog::link_log(CommitLogBase *log_base) {
                                          log_base->get_latest_revision());
   DynamicBuffer input;
   String &log_dir = log_base->get_log_dir();
+
+  if (m_fd == -1) {
+    try {
+      m_fd = m_fs->create(m_cur_fragment_fname, true, 8192, 3, 67108864);
+    }
+    catch (Hypertable::Exception &e) {
+      HT_ERRORF("Problem creating commit log fragment file '%s' - %s (%s)",
+                m_log_dir.c_str(), e.what(), Error::get_text(e.code()));
+      return e.code();
+    }
+  }
 
   input.ensure(header.length());
 
@@ -204,10 +227,10 @@ int CommitLog::close() {
     HT_ERRORF("Problem closing commit log file '%s' - %s (%s)",
               m_cur_fragment_fname.c_str(), e.what(),
               Error::get_text(e.code()));
+    m_fd = -1;
     return e.code();
   }
-
-  m_fd = 0;
+  m_fd = -1;
 
   return Error::OK;
 }
@@ -255,8 +278,17 @@ int CommitLog::roll() {
     return Error::OK;
 
   try {
+    if (m_fd > 0)
+      m_fs->close(m_fd);
+  }
+  catch (Exception &e) {
+    HT_ERRORF("Problem closing commit log fragment: %s: %s",
+              m_cur_fragment_fname.c_str(), e.what());
+  }
 
-    m_fs->close(m_fd);
+  m_fd = -1;
+
+  try {
 
     file_info.log_dir = m_log_dir;
     file_info.num = m_cur_fragment_num;
@@ -284,6 +316,7 @@ int CommitLog::roll() {
   catch (Exception &e) {
     HT_ERRORF("Problem rolling commit log: %s: %s",
               m_cur_fragment_fname.c_str(), e.what());
+    m_fd = -1;
     return e.code();
   }
 

src/cc/Hypertable/RangeServer/Range.cc
@@ -696,7 +696,15 @@ void Range::split_notify_master() {
 
 
 void Range::compact(bool major) {
-  run_compaction(major);
+  try {
+    run_compaction(major);
+  }
+  catch (Exception &e) {
+    HT_ERROR_OUT << e << HT_END;
+    m_maintenance_in_progress = false;
+    throw;
+  }
+
   m_maintenance_in_progress = false;
 }
 

src/cc/Hypertable/RangeServer/RangeServer.cc
@@ -745,9 +745,12 @@ RangeServer::load_range(ResponseCallback *cb, const TableIdentifier *table,
   try {
 
     /** Get TableInfo, create if doesn't exist **/
-    if (!m_live_map->get(table->id, table_info)) {
-      table_info = new TableInfo(m_master_client, table, schema);
-      register_table = true;
+    {
+      ScopedLock lock(m_mutex);
+      if (!m_live_map->get(table->id, table_info)) {
+        table_info = new TableInfo(m_master_client, table, schema);
+        register_table = true;
+      }
     }
 
     // Verify schema, this will create the Schema object and add it to

commit f2b3003ef0c74c47b7a83bbfe0a2d14c0fc7fbe3

Fixed problem with LocalBroker where it was closing the wrong descriptor because of reuse
- Fixed a CommitLog serialization problem introduced with commit a58dce91b4059b022921287a43d7afb669380d0b
- Augmented multiple-maintenance-threads test to run a "SELECT *" in the background once per second
- Also fixed some problems with the AccessGroup cleanup in the last checkin

src/cc/DfsBroker/local/LocalBroker.cc
@@ -46,6 +46,8 @@ extern "C" {
 
 using namespace Hypertable;
 
+atomic_t LocalBroker::ms_next_fd = ATOMIC_INIT(0);
+
 LocalBroker::LocalBroker(PropertiesPtr &cfg) {
   m_verbose = cfg->get_bool("verbose");
 
@@ -72,7 +74,7 @@ LocalBroker::~LocalBroker() {
 
 void
 LocalBroker::open(ResponseCallbackOpen *cb, const char *fname, uint32_t bufsz) {
-  int fd;
+  int fd, local_fd;
   String abspath;
 
   HT_DEBUGF("open file='%s' bufsz=%d", fname, bufsz);
@@ -82,12 +84,12 @@ LocalBroker::open(ResponseCallbackOpen *cb, const char *fname, uint32_t bufsz) {
   else
     abspath = m_rootdir + "/" + fname;
 
-  //fd = atomic_inc_return(&ms_unique_id);
+  fd = atomic_inc_return(&ms_next_fd);
 
   /**
    * Open the file
    */
-  if ((fd = ::open(abspath.c_str(), O_RDONLY)) == -1) {
+  if ((local_fd = ::open(abspath.c_str(), O_RDONLY)) == -1) {
     HT_ERRORF("open failed: file='%s' - %s", abspath.c_str(), strerror(errno));
     report_error(cb);
     return;
@@ -95,7 +97,7 @@ LocalBroker::open(ResponseCallbackOpen *cb, const char *fname, uint32_t bufsz) {
 
   {
     struct sockaddr_in addr;
-    OpenFileDataLocalPtr fdata(new OpenFileDataLocal(fd, O_RDONLY));
+    OpenFileDataLocalPtr fdata(new OpenFileDataLocal(local_fd, O_RDONLY));
 
     cb->get_address(addr);
 
@@ -109,7 +111,7 @@ LocalBroker::open(ResponseCallbackOpen *cb, const char *fname, uint32_t bufsz) {
 void
 LocalBroker::create(ResponseCallbackOpen *cb, const char *fname, bool overwrite,
                     int32_t bufsz, int16_t replication, int64_t blksz) {
-  int fd;
+  int fd, local_fd;
   int flags;
   String abspath;
 
@@ -121,7 +123,7 @@ LocalBroker::create(ResponseCallbackOpen *cb, const char *fname, bool overwrite,
   else
     abspath = m_rootdir + "/" + fname;
 
-  //fd = atomic_inc_return(&ms_unique_id);
+  fd = atomic_inc_return(&ms_next_fd);
 
   if (overwrite)
     flags = O_WRONLY | O_CREAT | O_TRUNC;
@@ -131,16 +133,16 @@ LocalBroker::create(ResponseCallbackOpen *cb, const char *fname, bool overwrite,
   /**
    * Open the file
    */
-  if ((fd = ::open(abspath.c_str(), flags, 0644)) == -1) {
+  if ((local_fd = ::open(abspath.c_str(), flags, 0644)) == -1) {
     HT_ERRORF("open failed: file='%s' - %s", abspath.c_str(), strerror(errno));
     report_error(cb);
     return;
   }
-  HT_DEBUGF("created file='%s' fd=%d", fname, fd);
+  HT_DEBUGF("created file='%s' fd=%d local_fd=%d", fname, fd, local_fd);
 
   {
     struct sockaddr_in addr;
-    OpenFileDataLocalPtr fdata(new OpenFileDataLocal(fd, O_WRONLY));
+    OpenFileDataLocalPtr fdata(new OpenFileDataLocal(local_fd, O_WRONLY));
 
     cb->get_address(addr);
 

src/cc/DfsBroker/local/LocalBroker.h
@@ -28,6 +28,7 @@ extern "C" {
 #include <unistd.h>
 }
 
+#include "Common/atomic.h"
 #include "Common/String.h"
 #include "Common/atomic.h"
 #include "Common/Properties.h"
@@ -98,6 +99,8 @@ namespace Hypertable {
 
 
   private:
+
+    static atomic_t ms_next_fd;
 
     virtual void report_error(ResponseCallback *cb);
 

src/cc/Hypertable/Lib/CommitLog.cc
@@ -138,6 +138,7 @@ int CommitLog::write(DynamicBuffer &buffer, int64_t revision) {
   BlockCompressionHeaderCommitLog header(MAGIC_DATA, revision);
 
   if (m_needs_roll) {
+    ScopedLock lock(m_mutex);
     if ((error = roll()) != Error::OK)
       return error;
   }
@@ -168,6 +169,7 @@ int CommitLog::link_log(CommitLogBase *log_base) {
   String &log_dir = log_base->get_log_dir();
 
   if (m_needs_roll) {
+    ScopedLock lock(m_mutex);
     if ((error = roll()) != Error::OK)
       return error;
   }

src/cc/Hypertable/RangeServer/AccessGroup.cc
@@ -123,25 +123,33 @@ int AccessGroup::add(const Key &key, const ByteString value) {
 
 
 CellListScanner *AccessGroup::create_scanner(ScanContextPtr &scan_context_ptr) {
-  ScopedLock lock(m_mutex);
   MergeScanner *scanner = new MergeScanner(scan_context_ptr);
+  std::vector<String> filenames;
 
-  while (m_scanners_blocked)
-    m_scanner_blocked_cond.wait(lock);
+  {
+    ScopedLock lock(m_mutex);
 
-  scanner->add_scanner(m_cell_cache_ptr->create_scanner(scan_context_ptr));
-  if (m_immutable_cache_ptr)
-    scanner->add_scanner(m_immutable_cache_ptr->create_scanner(
-                         scan_context_ptr));
-  if (!m_in_memory) {
-    CellStoreReleaseCallback callback(this);
-    for (size_t i=0; i<m_stores.size(); i++) {
-      scanner->add_scanner(m_stores[i]->create_scanner(scan_context_ptr));
-      callback.add_file( m_stores[i]->get_filename() );
+    while (m_scanners_blocked)
+      m_scanner_blocked_cond.wait(lock);
+
+    scanner->add_scanner(m_cell_cache_ptr->create_scanner(scan_context_ptr));
+    if (m_immutable_cache_ptr)
+      scanner->add_scanner(m_immutable_cache_ptr->create_scanner(
+                                                                 scan_context_ptr));
+    if (!m_in_memory) {
+      CellStoreReleaseCallback callback(this);
+      for (size_t i=0; i<m_stores.size(); i++) {
+        scanner->add_scanner(m_stores[i]->create_scanner(scan_context_ptr));
+        callback.add_file( m_stores[i]->get_filename() );
+      }
+      filenames = callback.get_file_vector();
+      scanner->install_release_callback(callback);
     }
-    m_file_tracker.add_references(callback.get_file_vector());
-    scanner->install_release_callback(callback);
   }
+
+  // This can't be done while holding m_mutex (causes deadlock)
+  m_file_tracker.add_references(filenames);
+
   return scanner;
 }
 

src/cc/Hypertable/RangeServer/LiveFileTracker.cc
@@ -78,39 +78,46 @@ void LiveFileTracker::update_files_column() {
   if (!m_need_update)
     return;
 
-  { 
-    ScopedLock lock(m_mutex);
-    String file_list;
+  String file_list;
+  String end_row;
 
-    for (std::set<String>::iterator iter = m_live.begin(); iter != m_live.end(); iter++)
-      file_list += (*iter) + ";\n";
+  m_mutex.lock();
+
+  for (std::set<String>::iterator iter = m_live.begin(); iter != m_live.end(); iter++)
+    file_list += (*iter) + ";\n";
       
-    m_blocked.clear();
-    foreach(const FileRefCountMap::value_type &v, m_referenced) {
-      if (m_live.count(v.first) == 0) {
-        file_list += format("#%s;\n", v.first.c_str());
-        m_blocked.insert(v.first);
-      }
+  m_blocked.clear();
+  foreach(const FileRefCountMap::value_type &v, m_referenced) {
+    if (m_live.count(v.first) == 0) {
+      file_list += format("#%s;\n", v.first.c_str());
+      m_blocked.insert(v.first);
     }
-
-    try {
-      if (m_is_root) {
-        MetadataRoot metadata(m_schema_ptr);
-        metadata.write_files(m_ag_name, file_list);
-      }
-      else {
-        MetadataNormal metadata(&m_identifier, m_end_row);
-        metadata.write_files(m_ag_name, file_list);
-      }
-    }
-    catch (Hypertable::Exception &e) {
-      HT_ERROR_OUT <<"Problem updating 'Files' column of METADATA: "
-                   << e << HT_END;
-      HT_THROW2(e.code(), e, (String)"Problem updating 'Files' column of METADATA: ");
-    }
-
-    m_need_update = false;
   }
+
+  end_row = m_end_row;
+
+  m_update_mutex.lock();
+  m_mutex.unlock();
+
+  try {
+    if (m_is_root) {
+      MetadataRoot metadata(m_schema_ptr);
+      metadata.write_files(m_ag_name, file_list);
+    }
+    else {
+      MetadataNormal metadata(&m_identifier, end_row);
+      metadata.write_files(m_ag_name, file_list);
+    }
+  }
+  catch (Hypertable::Exception &e) {
+    m_update_mutex.unlock();
+    HT_ERROR_OUT <<"Problem updating 'Files' column of METADATA: "
+                 << e << HT_END;
+    HT_THROW2(e.code(), e, (String)"Problem updating 'Files' column of METADATA: ");
+  }
+
+  m_need_update = false;
+  m_update_mutex.unlock();
 
 }
 

src/cc/Hypertable/RangeServer/LiveFileTracker.h
@@ -98,6 +98,7 @@ namespace Hypertable {
 
   private:
     Mutex            m_mutex;
+    Mutex            m_update_mutex;
     TableIdentifierManaged m_identifier;
     SchemaPtr        m_schema_ptr;
     String           m_start_row;

tests/integration/multiple-maintenance-threads/dump-loop.sh
@@ -1,0 +1,9 @@
+#!/bin/bash
+
+HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
+SCRIPT_DIR=`dirname $0`
+
+for ((i=0; i<20; i++)) ; do
+    sleep 1
+    $HT_HOME/bin/hypertable --batch < $SCRIPT_DIR/dump-table.hql > /dev/null
+done

tests/integration/multiple-maintenance-threads/run.sh
@@ -4,33 +4,43 @@ HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
 SCRIPT_DIR=`dirname $0`
 DATA_SIZE=${DATA_SIZE:-"100000000"}
 THREADS=${THREADS:-"8"}
+ITERATIONS=${ITERATIONS:-"1"}
 
-$HT_HOME/bin/clean-database.sh
-$HT_HOME/bin/start-all-servers.sh local \
-    --Hypertable.RangeServer.Range.MaxBytes=2500K \
-    --Hypertable.RangeServer.AccessGroup.MaxMemory=250K \
-    --Hypertable.RangeServer.MaintenanceThreads=$THREADS \
-    --Hypertable.RangeServer.Timer.Interval=10k
+for ((i=0; i<$ITERATIONS; i++)) ; do
 
-$HT_HOME/bin/hypertable --no-prompt < $SCRIPT_DIR/create-table.hql
+    $HT_HOME/bin/clean-database.sh
+    $HT_HOME/bin/start-all-servers.sh local \
+        --Hypertable.RangeServer.Range.MaxBytes=2500K \
+        --Hypertable.RangeServer.AccessGroup.MaxMemory=250K \
+        --Hypertable.RangeServer.MaintenanceThreads=$THREADS \
+        --Hypertable.RangeServer.Timer.Interval=10k
 
-echo "=================================="
-echo "$THREADS Maintenence Threads WRITE test"
-echo "=================================="
-$HT_HOME/bin/random_write_test $DATA_SIZE
+    $HT_HOME/bin/hypertable --no-prompt < $SCRIPT_DIR/create-table.hql
 
-dump_it() {
-  $HT_HOME/bin/hypertable --batch < $SCRIPT_DIR/dump-table.hql | wc -l
-}
+    $SCRIPT_DIR/dump-loop.sh &
 
-count=`dump_it`
-expected=$((DATA_SIZE / 1000))
+    echo "=================================="
+    echo "$THREADS Maintenence Threads WRITE test"
+    echo "=================================="
+    $HT_HOME/bin/random_write_test $DATA_SIZE
 
-if test $count -eq $expected; then
-  exit 0 # success
-else
-  echo "Expected: $expected, got: $count"
-  # try dump again to see if it's a transient problem
-  echo "Second dump: `dump_it`"
-  exit 1
-fi
+    kill %1
+
+    dump_it() {
+        $HT_HOME/bin/hypertable --batch < $SCRIPT_DIR/dump-table.hql | wc -l
+    }
+
+    count=`dump_it`
+    expected=$((DATA_SIZE / 1000))
+
+    if test $count -ne $expected; then
+        echo "Expected: $expected, got: $count"
+        # try dump again to see if it's a transient problem
+        echo "Second dump: `dump_it`"
+        #exec 1>&-
+        #sleep 86400
+        exit 1
+    fi
+done
+
+exit 0