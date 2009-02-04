480276dfa65ddc4fba75d56456a22e42e1b5b849
Sanjit Jhala <sjhalaz@gmail.com>
2009-02-04 15:06:37 -0800

    Fixed Bloomfilter integration test run script.

--- tests/integration/bloomfilter/run.sh
+++ tests/integration/bloomfilter/run.sh
@@ -2,25 +2,26 @@
 
 HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
 SCRIPT_DIR=`dirname $0`
-DATA_SIZE=${DATA_SIZE:-"10000000"}
+DATA_SIZE=${DATA_SIZE:-"5000000"}
 AG_MAXFILES=1000
 AG_MAXMEM=250000
 
 restart_servers() {
   $HT_HOME/bin/clean-database.sh
-  $HT_HOME/bin/start-all-servers.sh --no-rangeserver local
-  $HT_HOME/bin/start-rangeserver.sh \
-    --Hypertable.RangeServer.AccessGroup.MaxFiles=$AG_MAXFILES \
-    --Hypertable.RangeServer.AccessGroup.MaxMemory=$AG_MAXMEM
+  $HT_HOME/bin/start-all-servers.sh \
+      local \
+      --Hypertable.RangeServer.AccessGroup.MaxFiles=$AG_MAXFILES \
+      --Hypertable.RangeServer.AccessGroup.MaxMemory=$AG_MAXMEM \
+      --Hypertable.Lib.Mutator.FlushDelay=10
 }
 
 restart_servers_noclean() {
   $HT_HOME/bin/stop-servers.sh
-  $HT_HOME/bin/start-all-servers.sh --no-rangeserver local
-  $HT_HOME/bin/start-rangeserver.sh \
-    --Hypertable.RangeServer.AccessGroup.MaxFiles=$AG_MAXFILES \
-    --Hypertable.RangeServer.AccessGroup.MaxMemory=$AG_MAXMEM
-
+  $HT_HOME/bin/start-all-servers.sh \
+      local \
+      --Hypertable.RangeServer.AccessGroup.MaxFiles=$AG_MAXFILES \
+      --Hypertable.RangeServer.AccessGroup.MaxMemory=$AG_MAXMEM \
+      --Hypertable.Lib.Mutator.FlushDelay=10
 }
 
 test() {
@@ -29,23 +30,30 @@ test() {
   filter_type=$1
   echo "Run test for Bloom filter of type ${filter_type}"
 
-  $HT_HOME/bin/hypertable --no-prompt < $SCRIPT_DIR/create-bloom-${filter_type}-table.hql
+  $HT_HOME/bin/hypertable --no-prompt < \
+      $SCRIPT_DIR/create-bloom-${filter_type}-table.hql
   
   echo "================="
   echo "random WRITE test"
   echo "================="
-  $HT_HOME/bin/random_write_test --blocksize=100 $DATA_SIZE
+  $HT_HOME/bin/random_write_test \
+      --blocksize=100 \
+            $DATA_SIZE
   
   echo "================="
   echo "random READ test"
   echo "================="
-  $HT_HOME/bin/random_read_test $DATA_SIZE
+  $HT_HOME/bin/random_read_test \
+      --blocksize=100 \
+      $DATA_SIZE
 
   restart_servers_noclean  
   echo "================="
   echo "random READ test"
   echo "================="
-  $HT_HOME/bin/random_read_test --blocksize=100 $DATA_SIZE 
+  $HT_HOME/bin/random_read_test \
+      --blocksize=100 \
+      $DATA_SIZE 
 }
 
 test $1
