ec50d7a4663c0c880dd6241d03ad520bae78fb6c
Luke <luke@vicaya.(none)>
2009-07-18 17:37:22 -0700

    Restart test servers automatically every time when make *tests - Many integration tests run their servers with special configs.

--- bin/start-test-servers.sh
+++ bin/start-test-servers.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-[ $# -gt 1 ] || { echo "Usage: $0 <install_dir> <file_to_touch>"; exit 1; }
+[ $# -gt 1 ] || { echo "Usage: $0 <install_dir>"; exit 1; }
 
 INSTALL_DIR=$1
-TOUCH_FILE=$2
 
 $INSTALL_DIR/bin/clean-database.sh
-$INSTALL_DIR/bin/start-all-servers.sh local && touch $TOUCH_FILE
+$INSTALL_DIR/bin/start-all-servers.sh local
