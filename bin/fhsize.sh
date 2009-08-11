7bf0f118ea0a697f7f3e636f7f12ab860b8d77a5
Luke <luke@vicaya.(none)>
2009-08-10 18:14:15 -0700

    Added post install scripts to FHSize the directory layout

---
+++ bin/fhsize.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+#
+# Copyright (C) 2009  Luke Lu (llu@hypertable.org)
+#
+# This file is part of Hypertable.
+#
+# Hypertable is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or any later version.
+#
+# Hypertable is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hypertable. If not, see <http://www.gnu.org/licenses/>
+#
+
+# Post install script to FHSize the running layout according FHS
+# currently FHS 2.3: http://www.pathname.com/fhs/pub/fhs-2.3.html 
+
+export HYPERTABLE_HOME=$(cd `dirname "$0"`/.. && pwd)
+version=`basename $HYPERTABLE_HOME`
+
+# Variable/runtime data in /var/opt 
+echo "Setting up /var/opt/hypertable..."
+varhome=/var/opt/hypertable/$version
+varlink=/var/opt/hypertable/current
+
+[ -d $varhome ] && { echo "Already FHSized: $HYPERTABLE_HOME"; exit 0; }
+
+mkdir -p $varhome/hyperspace $varhome/fs $varhome/run $varhome/log
+rm -f $varlink && ln -s $varhome $varlink
+ln -sf $varhome/{hyperspace,fs,run,log} $HYPERTABLE_HOME
+
+# Config files in /etc/opt
+echo "Setting up /etc/opt/hypertable..."
+etchome=/etc/opt/hypertable/$version
+etclink=/etc/opt/hypertable/current
+mkdir -p $etchome
+cp $HYPERTABLE_HOME/conf/* $etchome &&
+    rm -rf $HYPERTABLE_HOME/conf && ln -s $etchome $HYPERTABLE_HOME/conf
+rm -f $etclink && ln -s $etchome $etclink
