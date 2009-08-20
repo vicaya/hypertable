6d8a59fad8fb8c9d7edf6a2965f036fff6d8c410
Luke <luke@vicaya.(none)>
2009-08-20 03:27:58 -0700

    Added a few convenient commands for ht for shared lib diagnosis

---
+++ bin/ldd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2009  Luke Lu (llu@hypertable.org)
+#
+# This file is part of Hypertable.
+#
+# Hypertable is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or any later version.
+#
+# Hypertable is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hypertable. If not, see <http://www.gnu.org/licenses/>
+#
+
+# Do ldd on several platforms
+export HYPERTABLE_HOME=$(cd `dirname "$0"`/.. && pwd)
+. $HYPERTABLE_HOME/bin/ht-env.sh
+
+case `uname -s` in
+  Darwin)     ldd='otool -L';;
+  *)          ldd=ldd;;
+esac
+case $1 in
+  /*)         file=$1;;
+  lib/*)      file=$HYPERTABLE_HOME/lib/$1;;
+  *)          file=$HYPERTABLE_HOME/bin/$1;;
+esac
+
+exec $ldd "$file"
