04e5a62deb11c0118234bcff0800980fc1a5106f
Luke Lu <llu@hypertable.org>
2009-09-19 03:10:55 -0700

    Fixed tcmalloc version check (should run every time)

--- cmake/FindTcmalloc.cmake
+++ cmake/FindTcmalloc.cmake
@@ -5,12 +5,6 @@
 #  Tcmalloc_LIBRARIES   - List of libraries when using Tcmalloc.
 #  Tcmalloc_FOUND       - True if Tcmalloc found.
 
-
-if (Tcmalloc_INCLUDE_DIR)
-  # Already in cache, be silent
-  set(Tcmalloc_FIND_QUIETLY TRUE)
-endif ()
-
 find_path(Tcmalloc_INCLUDE_DIR google/tcmalloc.h
   /opt/local/include
   /usr/local/include
@@ -31,29 +25,27 @@ else ()
 endif ()
 
 if (Tcmalloc_FOUND)
-  if (NOT Tcmalloc_FIND_QUIETLY)
-    message(STATUS "Found Tcmalloc: ${Tcmalloc_LIBRARY}")
-    try_run(TC_CHECK TC_CHECK_BUILD
-            ${HYPERTABLE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
-            ${HYPERTABLE_SOURCE_DIR}/cmake/CheckTcmalloc.cc
-            CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${Tcmalloc_INCLUDE_DIR}
-                        -DLINK_LIBRARIES=${Tcmalloc_LIBRARIES}
-            OUTPUT_VARIABLE TC_TRY_OUT)
-    #message("tc_check build: ${TC_CHECK_BUILD}")
-    #message("tc_check: ${TC_CHECK}")
-    #message("tc_version: ${TC_TRY_OUT}")
-    if (TC_CHECK_BUILD AND NOT TC_CHECK STREQUAL "0")
-      string(REGEX REPLACE ".*\n(Tcmalloc .*)" "\\1" TC_TRY_OUT ${TC_TRY_OUT})
-      message(STATUS "${TC_TRY_OUT}")
-      message(FATAL_ERROR "Please fix the tcmalloc installation and try again.")
-      set(Tcmalloc_LIBRARIES)
-    endif ()
-    string(REGEX REPLACE ".*\n([0-9]+[^\n]+).*" "\\1" TC_VERSION ${TC_TRY_OUT})
-    if (NOT TC_VERSION MATCHES "^[0-9]+.*")
-      set(TC_VERSION "unknown -- make sure it's 1.1+")
-    endif ()
-    message(STATUS "       version: ${TC_VERSION}")
+  message(STATUS "Found Tcmalloc: ${Tcmalloc_LIBRARY}")
+  try_run(TC_CHECK TC_CHECK_BUILD
+          ${HYPERTABLE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp
+          ${HYPERTABLE_SOURCE_DIR}/cmake/CheckTcmalloc.cc
+          CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${Tcmalloc_INCLUDE_DIR}
+                      -DLINK_LIBRARIES=${Tcmalloc_LIBRARIES}
+          OUTPUT_VARIABLE TC_TRY_OUT)
+  #message("tc_check build: ${TC_CHECK_BUILD}")
+  #message("tc_check: ${TC_CHECK}")
+  #message("tc_version: ${TC_TRY_OUT}")
+  if (TC_CHECK_BUILD AND NOT TC_CHECK STREQUAL "0")
+    string(REGEX REPLACE ".*\n(Tcmalloc .*)" "\\1" TC_TRY_OUT ${TC_TRY_OUT})
+    message(STATUS "${TC_TRY_OUT}")
+    message(FATAL_ERROR "Please fix the tcmalloc installation and try again.")
+    set(Tcmalloc_LIBRARIES)
   endif ()
+  string(REGEX REPLACE ".*\n([0-9]+[^\n]+).*" "\\1" TC_VERSION ${TC_TRY_OUT})
+  if (NOT TC_VERSION MATCHES "^[0-9]+.*")
+    set(TC_VERSION "unknown -- make sure it's 1.1+")
+  endif ()
+  message(STATUS "       version: ${TC_VERSION}")
 else ()
   message(STATUS "Not Found Tcmalloc: ${Tcmalloc_LIBRARY}")
   if (Tcmalloc_FIND_REQUIRED)
