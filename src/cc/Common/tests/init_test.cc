22545cb00bfe2377bbb3529555528f2e57b9b002
Luke Lu <llu@hypertable.org>
2009-10-15 18:06:20 -0700

    Added basic regression test init_* function. - Caught int32_t masking issues on Solaris

---
+++ src/cc/Common/tests/init_test.cc
@@ -0,0 +1,32 @@
+#include "Common/Compat.h"
+#include "Common/Init.h"
+
+using namespace Hypertable;
+using namespace Hypertable::Config;
+
+namespace {
+
+struct AppPolicy : Policy {
+  static void init_options() {
+    cmdline_desc("Usage: %s [Options]\nOptions").add_options()
+      ("i16", i16(), "16-bit integer")
+      ("i32", i32(), "32-bit integer")
+      ("i64", i64(), "64-bit integer")
+      ("i64s", i64s(), "a list of 64-bit integers")
+      ("boo", boo()->zero_tokens()->default_value(false), "a boolean arg")
+      ("str", str(), "a string arg")
+      ("strs", strs(), "a list of strings")
+      ("f64", f64(), "a double arg")
+      ("f64s", f64s(), "a list of doubles")
+      ;
+  }
+};
+
+typedef Meta::list<AppPolicy, DefaultPolicy> Policies;
+
+} // local namespace
+
+int main(int argc, char *argv[]) {
+  init_with_policies<Policies>(argc, argv);
+  Config::properties->print(std::cout);
+}
