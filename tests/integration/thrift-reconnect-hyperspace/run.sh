0196dd4874c10e5a07d19aea7a4c5413fce5d791
Sanjit Jhala <sjhalaz@gmail.com>
2010-07-29 14:50:09 -0700

    [Issue 481] ThriftClient-reconnect-hyperspace failure. Fixed.

--- tests/integration/thrift-reconnect-hyperspace/run.sh
+++ tests/integration/thrift-reconnect-hyperspace/run.sh
@@ -8,10 +8,11 @@ HT_HOME=${INSTALL_DIR:-"$HOME/hypertable/current"}
 $HT_HOME/bin/clean-database.sh
 $HT_HOME/bin/start-all-servers.sh local --Hyperspace.Session.Reconnect=true
 
-sleep 2;
+sleep 7;
 $HT_HOME/bin/stop-servers.sh --no-thriftbroker 
+sleep 7;
 $HT_HOME/bin/start-all-servers.sh --no-thriftbroker local
-sleep 10;
+sleep 7;
 
 cd ${THRIFT_CPP_TEST_DIR};
 ${TEST_BIN}
