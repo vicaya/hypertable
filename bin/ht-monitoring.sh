1affccaeba0147ded449a0ed2810c31c618cb39d
defun <harsha@defun.org>
2010-09-21 17:01:12 -0700

    shell script changes

--- bin/ht-monitoring.sh
+++ bin/ht-monitoring.sh
@@ -1,21 +1,3 @@
-#!/usr/bin/env bash
-#
-# Copyright 2010 Sanjit Jhala (Hypertable, Inc.)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# The installation directory
 export HYPERTABLE_HOME=$(cd `dirname "$0"`/.. && pwd)
 
 port=38090
@@ -23,18 +5,17 @@ pidfile="$HYPERTABLE_HOME/run/MonitoringServer.pid"
 log="$HYPERTABLE_HOME/log/MonitoringServer.log"
 monitoring_script_dir="$HYPERTABLE_HOME/Monitoring"
 config_file="${monitoring_script_dir}/app/config/config.yml"
+rack_file="${monitoring_script_dir}/config.ru"
 
 start_monitoring() {
   cd ${monitoring_script_dir}
-  echo ":production:
-        :hypertable_home: \"${HYPERTABLE_HOME}\"" > ${config_file}
-  
-  command="thin -c ${HYPERTABLE_HOME}-p ${port} -e production -P ${pidfile} -l ${log} -d -R config.ru start"
+  command="thin -p ${port} -e production -P ${pidfile} -l ${log}  -R ${rack_file} -d start"
   $command 
 }
 
 stop_monitoring() {
   cd ${monitoring_script_dir}
-  command="thin -p ${port} -e production -P ${pidfile} -l ${log}  -d -R config.ru stop"
+  command="thin -p ${port} -e production -P ${pidfile} -l ${log} -R ${rack_file} -d stop"
   $command
 }
+
