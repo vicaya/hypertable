f46807a6171ae8650430048f79fa453a114ef67a
defun <harsha@defun.org>
2010-10-25 11:56:47 -0700

    issues with config

--- src/rb/Monitoring/htmonitoring.rb
+++ src/rb/Monitoring/htmonitoring.rb
@@ -13,9 +13,12 @@ Dir["#{  File.dirname(__FILE__)}/app/lib/data/*.rb"].each {|r| require r}
 
 module HTMonitoring
   @root = Pathname.new(File.dirname(__FILE__)).expand_path
+  @hypertable_home = ENV['HYPERTABLE_HOME']
 
   def self.config
     @config ||= YAML.load_file(@root.join("app/config/config.yml"))[:production]
+    @config[:data] = eval "\"#{@config[:data]}\""
+    @config
   end
 
   def self.tablestats
